74232443121ffe555dccec663e5d29768c7070a2
Andrew Gaete <andrewgaete@andrews-imac.lan>
2018-12-22 10:34:16 -0600

    Add update script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -31,8 +31,8 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //homeWork findOneAndDelete
 //db.collection('Users').deleteMany({name: 'Andrew'});
 
-db.collection('Users').findOneAndDelete({
-  _id: new ObjectID("5c1dafef2d792c0b9f544b11")
-}).then((results) => {
-  console.log(JSON.stringify(results, undefined, 2));
-});
+// db.collection('Users').findOneAndDelete({
+//   _id: new ObjectID("5c1dafef2d792c0b9f544b11")
+// }).then((results) => {
+//   console.log(JSON.stringify(results, undefined, 2));
+// });
